9f3a3e6c04c028ed424f9d28e296b1d1686911d3
HnTRdevelop <sergeisiviz@gmail.com>
2022-11-17 17:51:25 +0500

    vectors expand

--- engine.py
+++ engine.py
@@ -1,20 +1,54 @@
 import taichi as ti
+import math
 
 
 class Vector2:
-    def __init__(self, x: float, y: float) -> None:
+    def __init__(self, x: float = 0, y: float = 0) -> None:
         self.x = x
         self.y = y
     
     def get_tuple(self):
         return (self.x, self.y)
-        
+
+    def __add__(self, other):
+        return Vector2(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other):
+        return Vector2(self.x - other.x, self.y - other.y)
+    
+    def __str__(self):
+        return f"({self.x}, {self.y})"
+
+    def dot(self, other):
+        return self.x * other.x + self.y * other.y
+
+    def distance(self, other):
+        return math.sqrt((self.x - other.x) ** 2 + (self.y - other.y) ** 2)
+
 
 class Vector3:
-    def __init__(self, x: float, y: float, z: float) -> None:
+    def __init__(self, x: float = 0, y: float = 0, z: float = 0) -> None:
         self.x = x
         self.y = y
         self.z = z
     
     def get_tuple(self):
         return (self.x, self.y, self.z)
+    
+    def __add__(self, other):
+        return Vector3(self.x + other.x, self.y + other.y, self.z + other.z)
+
+    def __sub__(self, other):
+        return Vector3(self.x - other.x, self.y - other.y, self.z - other.z)
+    
+    def __mul__(self, num: float):
+        return Vector3(self.x * num, self.y * num, self.z * num)
+
+    def __str__(self):
+        return f"({self.x}, {self.y}, {self.z})"
+
+    def dot(self, other):
+        return self.x * other.x + self.y * other.y + self.z * other.z
+
+    def distance(self, other):
+        return math.sqrt((self.x - other.x) ** 2 + (self.y - other.y) ** 2 + (self.z - other.z) ** 2)
