5bca863d81e047217f77b5f9ee1424993afb06aa
Kira <116455577+kiraQueenBomb3@users.noreply.github.com>
2023-01-22 14:47:51 +0500

    fourth part of counting
    хз, вроде должно работать

--- physics.py
+++ physics.py
@@ -80,4 +80,15 @@ def counting_speed(m1, v1, m2, v2, chase=False, opposite=False):
             need_v1 = (impulse - m2 * need_v2) / m1
             return need_v1, need_v2
         else:
-            pass
+            need_v2_1 = (2 * (m2 ** 2 * v2 - m1 * m2 * v1) +
+                         math.sqrt(m2 ** 2 * v2 * (m2 - 1) * (m2 + m1) + (m1 * m2 * v1 + m1 * m2 * v2) ** 2)) / (
+                                    2 * m2 * (m2 + m1))
+            need_v2_2 = (2 * (m2 ** 2 * v2 - m1 * m2 * v1) -
+                         math.sqrt(m2 ** 2 * v2 * (m2 - 1) * (m2 + m1) + (m1 * m2 * v1 + m1 * m2 * v2) ** 2)) / (
+                                2 * m2 * (m2 + m1))
+            if not ((need_v2_1 < math.sqrt((m1 * v1 ** 2 + m2 * v2 ** 2) / m2)) and (need_v2_1 > impulse / m2)):
+                need_v2 = need_v2_2
+            else:
+                need_v2 = need_v2_1
+            need_v1 = (impulse - m2 * need_v2) / m1
+            return need_v1, need_v2
